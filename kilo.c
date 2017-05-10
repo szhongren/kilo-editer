b224364eddb1f2a072c4371a728258877d12d652
Zhongren Shao <shao.zhongren@gmail.com>
2017-05-10 12:26:06 -0400

    Added incremental search.

--- kilo.c
+++ kilo.c
@@ -73,7 +73,7 @@ struct editorConfig E;
 
 void editorSetStatusMessage(const char *fmt, ...);
 void editorRefreshScreen();
-char *editorPrompt(char *prompt);
+char *editorPrompt(char *prompt, void (*callback)(char *, int));
 
 /********** terminal **********/
 
@@ -419,7 +419,7 @@ void editorOpen(char *filename) {
 void editorSave() {
     // deal with new file
     if (E.filename == NULL) {
-        E.filename = editorPrompt("Save as :%s (ESC to cancel)");
+        E.filename = editorPrompt("Save as :%s (ESC to cancel)", NULL);
         if (E.filename == NULL) {
             editorSetStatusMessage("Save aborted");
             return;
@@ -449,16 +449,44 @@ void editorSave() {
 
 /********** search **********/
 
-void editorFind() {
-    char *query = editorPrompt("Search: %s (ESC to cancel)");
-    if (query == NULL) return;
+// called every time a character is entered in the find prompt
+void editorFindCallback(char *query, int key) {
+    // holds across invokations to hold the state of the find function
+    static int last_match = -1;
+    static int direction = 1;
 
+    if (key == '\r' || key == '\x1b') {
+        // reset
+        last_match = -1;
+        direction = 1;
+        return;
+    } else if (key == ARROW_RIGHT || key == ARROW_DOWN) {
+        direction = 1;
+    } else if (key == ARROW_LEFT || key == ARROW_UP) {
+        direction = -1;
+    } else {
+        // reset
+        last_match = -1;
+        direction = 1;
+    }
+
+    // start from the top of the file
+    if (last_match == -1) direction = 1;
+    int current = last_match;
     int i;
     for (i = 0; i < E.numrows; i++) {
-        erow *row = &E.row[i];
+        current += direction;
+        // handle top and bottom of file
+        if (current == -1)
+            current = E.numrows - 1;
+        else if (currrent == E.numrows)
+            current = 0;
+
+        erow *row = &E.row[current];
         char *match = strstr(row->render, query);
         if (match) {
-            E.cy = i;
+            last_match = current;
+            E.cy = current;
             E.cx = editorRowRxToCx(row, match - row->render);
             E.rowoff = E.numrows;
             break;
@@ -466,9 +494,27 @@ void editorFind() {
     }
 }
 
+void editorFind() {
+    int saved_cx = E.cx;
+    int saved_cy = E.cy;
+    int saved_coloff = E.coloff;
+    int saved_rowoff = E.rowoff;
+
+    char *query = editorPrompt("Search: %s (Use ESC/Arrows/Enter)", editorFindCallback);
+
+    if (query) {
+        free(query);
+    } else {
+        E.cx = saved_cx;
+        E.cy = saved_cy;
+        E.coloff = saved_coloff;
+        E.rowoff = saved_rowoff;
+    }
+}
+
 /********** input **********/
 
-char *editorPrompt(char *prompt) {
+char *editorPrompt(char *prompt, void (*callback)(char *, int)) {
     size_t bufsize = 128;
     char *buf = malloc(bufsize);
 
@@ -486,12 +532,14 @@ char *editorPrompt(char *prompt) {
         } else if (c == '\x1b') {
             // ESC
             editorSetStatusMessage("");
+            if (callback) callback(buf, c);
             free(buf);
             return NULL;
         } else if (c == '\r') {
             // Enter
             if (buflen != 0) {
                 editorSetStatusMessage("");
+                if (callback) callback(buf, c);
                 return buf;
             }
         } else if (!iscntrl(c) && c < 128) {
@@ -502,6 +550,7 @@ char *editorPrompt(char *prompt) {
             buf[buflen++] = c;
             buf[buflen] = '\0';
         }
+        if (callback) callback(buf, c);
     }
 }
 
